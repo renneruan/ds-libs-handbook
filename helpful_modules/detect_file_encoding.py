3d20b7d1458cd3f5d7b3ea64aaceff60d8eb0f4f
Rennê Ruan Alves Oliveira <renneruan@hotmail.com>
2024-07-03 09:02:00 -0300

    Renaming dataset "dados_bancarios" (bank data in PT-BR)

--- helpful_modules/detect_file_encoding.py
+++ helpful_modules/detect_file_encoding.py
@@ -4,8 +4,8 @@ import pandas as pd
 # %% Check encoding of given file
 import charset_normalizer
 
-with open("datasets/dataset_1.csv", "rb") as rawdata:
+with open("datasets/dados_bancarios.csv", "rb") as rawdata:
     result = charset_normalizer.detect(rawdata.read(10000))
 
 print(result)
-df_ascii = pd.read_csv("datasets/dataset_1.csv", encoding="ascii")
+df_ascii = pd.read_csv("datasets/dados_bancarios.csv", encoding="ascii")
--- helpful_modules/extract_string_matches.py
+++ helpful_modules/extract_string_matches.py
@@ -4,7 +4,7 @@ import fuzzywuzzy
 from fuzzywuzzy import process
 import pandas as pd
 
-df_ascii = pd.read_csv("../datasets/dataset_1.csv")
+df_ascii = pd.read_csv("../datasets/dados_bancarios.csv")
 matches = fuzzywuzzy.process.extract(
     "admin", df_ascii.emprego, limit=10, scorer=fuzzywuzzy.fuzz.token_sort_ratio
 )
